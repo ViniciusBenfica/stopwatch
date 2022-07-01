fb60f88c49aac5afd1705a75e984c480d03b374d
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-07-01 17:01:29 -0300

    creating styles file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,110 +1,20 @@
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
-import { styled } from '../styles/stitches.config'
 import * as Dialog from '@radix-ui/react-dialog';
 
-import {
-  css,
-} from '@stitches/react';
-
-const Container = styled('div', {
-  display: "flex",
-})
-
-const DivTimer = styled('div', {
-  display: 'flex',
-  alignItems: 'center',
-  margin: "15px 0",
-  // all: 'unset',
-  // gap: 20,
-})
-
-const Input = styled('input', {
-  all: 'unset',
-  width: '290px',
-  height: "35px",
-  fontSize: 15,
-  color: "$testando",
-  boxShadow: `0 0 0 1px #470070`,
-  borderRadius: "5px",
-
-  '&:focus': { boxShadow: `0 0 0 2px #470070` },
-
-})
-
-const Label = styled('label', {
-  all: 'unset',
-  width: '90px',
-  textAlign: "right",
-  marginRight: "20px",
-  // boxShadow: `0 0 0 1px $testando`,
-  borderRadius: 4,
-  color: "$testando",
-  fontSize: "15px"
-
-})
-
-const Stopwatch = styled('div', {
-  width: '120px',
-  height: '120px',
-  lineHeight: "4",
-  textAlign: "center",
-  fontSize: "30px",
-  borderRadius: '50%',
-  background: `radial-gradient(white 50%, transparent 51%), conic-gradient(blue 250deg, orange 0deg)`,
-  margin: "0 10px",
-  variants: {
-    BackgroundColor: {
-      red: { backgroundColor: "red" }
-    }
-  }
-})
-
-const Overlay = styled(Dialog.Overlay, {
-  position: "fixed",
-  inset: 0,
-  backgroundColor: "rgba(0, 0, 0, 0.2)"
-})
-
-const Content = styled(Dialog.Content, {
-  position: "fixed",
-  width: "400px",
-  height: "250px",
-  background: "white",
-  padding: "20px",
-  borderRadius: "10px",
-  top: "50%",
-  left: "50%",
-  lineHeight: "1.5",
-  transform: "translate(-50%, -50%)",
-})
-
-const DivInputs = styled("div", {
-  // display: "flex",
-  // flexDirection: "column",
-  alignContent: "space-around",
-})
-
-const Close = styled(Dialog.Close, {
-  all: 'unset',
-  borderRadius: "5px",
-  padding: '0 15px',
-  fontSize: 15,
-  height: 35,
-  float: "right",
-  backgroundColor: "#33ABAA",
-  color: "white",
-  marginTop: "10px",
-
-  '&:hover': { backgroundColor: "#2d9999" },
-})
-
-const box = css({
-  
-});
+import { 
+  Container,
+  DivTimer,
+  Input,
+  Label,
+  Stopwatch,
+  Overlay,
+  Content,
+  DivInputs,
+  Close,
+  box} from "../styles/index.style"
 
 const Home: NextPage = () => {
-
 
   return (
     <Container>
---
+++ styles/index.style.tsx
@@ -0,0 +1,98 @@
+import { styled } from './stitches.config'
+import { css } from '@stitches/react';
+import * as Dialog from '@radix-ui/react-dialog';
+
+export const Container = styled('div', {
+    display: "flex",
+  })
+  
+export const DivTimer = styled('div', {
+    display: 'flex',
+    alignItems: 'center',
+    margin: "15px 0",
+    // all: 'unset',
+    // gap: 20,
+})
+
+export const Input = styled('input', {
+    all: 'unset',
+    width: '290px',
+    height: "35px",
+    fontSize: 15,
+    color: "$testando",
+    boxShadow: `0 0 0 1px #470070`,
+    borderRadius: "5px",
+
+    '&:focus': { boxShadow: `0 0 0 2px #470070` },
+
+})
+
+export const Label = styled('label', {
+    all: 'unset',
+    width: '90px',
+    textAlign: "right",
+    marginRight: "20px",
+    // boxShadow: `0 0 0 1px $testando`,
+    borderRadius: 4,
+    color: "$testando",
+    fontSize: "15px"
+})
+
+export const Stopwatch = styled('div', {
+    width: '120px',
+    height: '120px',
+    lineHeight: "4",
+    textAlign: "center",
+    fontSize: "30px",
+    borderRadius: '50%',
+    background: `radial-gradient(white 50%, transparent 51%), conic-gradient(blue 250deg, orange 0deg)`,
+    margin: "0 10px",
+    variants: {
+        BackgroundColor: {
+        red: { backgroundColor: "red" }
+        }
+    }
+})
+
+export const Overlay = styled(Dialog.Overlay, {
+    position: "fixed",
+    inset: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.2)"
+})
+
+export const Content = styled(Dialog.Content, {
+    position: "fixed",
+    width: "400px",
+    height: "250px",
+    background: "white",
+    padding: "20px",
+    borderRadius: "10px",
+    top: "50%",
+    left: "50%",
+    lineHeight: "1.5",
+    transform: "translate(-50%, -50%)",
+})
+
+export const DivInputs = styled("div", {
+    // display: "flex",
+    // flexDirection: "column",
+    alignContent: "space-around",
+})
+
+export const Close = styled(Dialog.Close, {
+    all: 'unset',
+    borderRadius: "5px",
+    padding: '0 15px',
+    fontSize: 15,
+    height: 35,
+    float: "right",
+    backgroundColor: "#33ABAA",
+    color: "white",
+    marginTop: "10px",
+
+    '&:hover': { backgroundColor: "#2d9999" },
+})
+
+export const box = css({
+
+});
