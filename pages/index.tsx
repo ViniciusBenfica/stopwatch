13090e883e29e39479575b80eb285d4012148d2b
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-07-03 17:50:12 -0300

    adding logic to stop the timer

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,34 +16,46 @@ import {
   Trigger
 } from "../styles/index.style"
 
+interface time {
+  seconds: number,
+  minutes: number,
+  hours: number
+}
+
 const Home: NextPage = () => {
 
-  var [count, setCount] = useState<number>(0)
   var [timer, setTimer] = useState<any>()
-
-  var [teste, setTeste] = useState<any>({ second: 0, minute: 0, hour: 0 })
-
+  var [count, setCount] = useState<time>({ seconds: 0, minutes: 0, hours: 0 })
+  var [start, setStart] = useState<boolean>(true)
 
   var startStopwatch = (opcao: boolean): any => {
-    var objectTime = { second: 0, minute: 0, hour: 0 }
-
+    var objectTime = count
     if (opcao) {
-      setTimer(window.setInterval(() => {
-        objectTime.second++
-        if (objectTime.second >= 60) {
-          objectTime.minute++
-          objectTime.second = 0
-        }
+      if (start) {
+        setTimer(window.setInterval(() => {
+          objectTime.seconds++
 
-        if (objectTime.minute >= 60) {
-          objectTime.hour++
-          objectTime.minute = 0
-        }
+          if (objectTime.seconds >= 60) {
+            objectTime.minutes++
+            objectTime.seconds = 0
+          }
 
-        setTeste({ second: objectTime.second, minute: objectTime.minute, hour: objectTime.hour })
-      }, 1000))
+          if (objectTime.minutes >= 60) {
+            objectTime.hours++
+            objectTime.minutes = 0
+          }
+
+          setCount({ seconds: objectTime.seconds, minutes: objectTime.minutes, hours: objectTime.hours })
+        }, 1000))
+        setStart(false)
+      } else {
+        setTimer(clearInterval(timer))
+        setStart(true)
+      }
+
     } else {
       setTimer(clearInterval(timer))
+      setCount({ seconds: 0, minutes: 0, hours: 0 })
     }
 
   }
@@ -54,23 +66,23 @@ const Home: NextPage = () => {
         <h1>STUDY TIMER</h1>
         <Timer>
           <div>
-            <Stopwatch BackgroundColor={'red'}>{teste.second}</Stopwatch>
+            <Stopwatch BackgroundColor={'red'}>{count.seconds}</Stopwatch>
             <div>HOURS</div>
           </div>
           <div>
-            <Stopwatch BackgroundColor={'red'}>{teste.minute}</Stopwatch>
+            <Stopwatch BackgroundColor={'red'}>{count.minutes}</Stopwatch>
             <div>MINUTES</div>
           </div>
           <div>
-            <Stopwatch BackgroundColor={'red'}>{teste.hour}</Stopwatch>
+            <Stopwatch BackgroundColor={'red'}>{count.hours}</Stopwatch>
             <div>SECONDS</div>
           </div>
         </Timer>
         <Dialog.Root>
           <div className={box({ css: { display: "flex", justifyContent: "space-around", width: "100%" } })}>
             <Trigger>Edit stopwatch</Trigger>
-            <Button onClick={() => startStopwatch(true)}>Start stopwatch</Button>
-            <Button onClick={() => startStopwatch(false)}>Stop stopwatch</Button>
+            {start ? <Button onClick={() => startStopwatch(true)}>Start stopwatch</Button> : <Button onClick={() => startStopwatch(true)}>Stop stopwatch</Button>}
+            <Button onClick={() => startStopwatch(false)}>Reset stopwatch</Button>
           </div>
           <Dialog.Portal>
             <Overlay />
